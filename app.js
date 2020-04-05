b7b8fabc0e892bb6c48b870d0d02ec6f09ffc7c4
kuzmich84 <kudim@inbox.ru>
2020-04-05 22:47:57 +0300

    add localstorage

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ const tasks = [
             '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
         },
     };
-let lastSelectedTheme = 'default';
+let lastSelectedTheme = localStorage.getItem('app_theme') || 'default';
 
     //Elements UI
     const listContainer = document.querySelector('.tasks-list-section .list-group');
@@ -111,6 +111,8 @@ let lastSelectedTheme = 'default';
     const inputTitle = form.elements['title'];
     const inputBody = form.elements['body'];
     const themeSelect = document.getElementById('themeSelect');
+
+    setTheme(lastSelectedTheme);
 
     renderAllTasks(objOfTasks);
 
@@ -219,6 +221,7 @@ let lastSelectedTheme = 'default';
         }
         setTheme(selectedTheme);
         lastSelectedTheme = selectedTheme;
+        localStorage.setItem('app_theme', selectedTheme);
     }
 
     function setTheme(name) {
